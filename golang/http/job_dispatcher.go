421de35d420ed11994f7587097362dc730238a9f
Antonio Schönmann <antonio_schonmann@hotmail.com>
2018-10-14 21:43:15 -0300

    Update job_dispatcher.go

--- golang/http/job_dispatcher.go
+++ golang/http/job_dispatcher.go
@@ -56,3 +56,34 @@ func main() {
 	})
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d",*serverPort), nil))
 }
+
+/*
+
+Loadtest results:
+
+[Sun Oct 14 2018 21:41:57 GMT-0300 (Brasilia Standard Time)] INFO Requests: 0, requests per second: 0, mean latency: 0 ms
+[Sun Oct 14 2018 21:42:02 GMT-0300 (Brasilia Standard Time)] INFO Requests: 4568, requests per second: 914, mean latency: 3.5 ms
+[Sun Oct 14 2018 21:42:07 GMT-0300 (Brasilia Standard Time)] INFO Requests: 9567, requests per second: 1000, mean latency: 3 ms
+[Sun Oct 14 2018 21:42:12 GMT-0300 (Brasilia Standard Time)] INFO Requests: 14567, requests per second: 1000, mean latency: 2.8 ms
+[Sun Oct 14 2018 21:42:17 GMT-0300 (Brasilia Standard Time)] INFO
+[Sun Oct 14 2018 21:42:17 GMT-0300 (Brasilia Standard Time)] INFO Target URL:          http://localhost:8787/work
+[Sun Oct 14 2018 21:42:17 GMT-0300 (Brasilia Standard Time)] INFO Max time (s):        20
+[Sun Oct 14 2018 21:42:17 GMT-0300 (Brasilia Standard Time)] INFO Concurrency level:   100
+[Sun Oct 14 2018 21:42:17 GMT-0300 (Brasilia Standard Time)] INFO Agent:               none
+[Sun Oct 14 2018 21:42:17 GMT-0300 (Brasilia Standard Time)] INFO Requests per second: 1000
+[Sun Oct 14 2018 21:42:17 GMT-0300 (Brasilia Standard Time)] INFO
+[Sun Oct 14 2018 21:42:17 GMT-0300 (Brasilia Standard Time)] INFO Completed requests:  19565
+[Sun Oct 14 2018 21:42:17 GMT-0300 (Brasilia Standard Time)] INFO Total errors:        0
+[Sun Oct 14 2018 21:42:17 GMT-0300 (Brasilia Standard Time)] INFO Total time:          20.002837615 s
+[Sun Oct 14 2018 21:42:17 GMT-0300 (Brasilia Standard Time)] INFO Requests per second: 978
+[Sun Oct 14 2018 21:42:17 GMT-0300 (Brasilia Standard Time)] INFO Mean latency:        3 ms
+[Sun Oct 14 2018 21:42:17 GMT-0300 (Brasilia Standard Time)] INFO
+[Sun Oct 14 2018 21:42:17 GMT-0300 (Brasilia Standard Time)] INFO Percentage of the requests served within a certain time
+[Sun Oct 14 2018 21:42:17 GMT-0300 (Brasilia Standard Time)] INFO   50%      2 ms
+[Sun Oct 14 2018 21:42:17 GMT-0300 (Brasilia Standard Time)] INFO   90%      5 ms
+[Sun Oct 14 2018 21:42:17 GMT-0300 (Brasilia Standard Time)] INFO   95%      6 ms
+[Sun Oct 14 2018 21:42:17 GMT-0300 (Brasilia Standard Time)] INFO   99%      8 ms
+[Sun Oct 14 2018 21:42:17 GMT-0300 (Brasilia Standard Time)] INFO  100%      21 ms (longest request)
+
+*/
+
